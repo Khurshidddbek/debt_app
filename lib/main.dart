e9ce4bdaf29843e82687e8d1650bb81d0a6ec32b
Khurshidddbek <Khurshidddbek@gmail.com>
2021-12-23 10:53:24 +0500

    The responsiveness of Registration_page has been fixed. Google and Apple logos have been added for registration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qarz_app/model/contact_model.dart';
 import 'package:qarz_app/pages/Authentication/registration_page.dart';
 import 'package:qarz_app/pages/add_debt_page.dart';
@@ -17,31 +18,34 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Your debt',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      builder: () => MaterialApp(
+        title: 'Your debt',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RegistrationPage(),
+        routes: {
+          // Authentication
+          RegistrationPage.id: (context) => RegistrationPage(),
+          SignInPage.id: (context) => SignInPage(),
+          SignUpPage.id: (context) => SignUpPage(),
+
+          // Home
+          HomePage.id: (context) => HomePage(),
+          CreateContactPage.id: (context) => CreateContactPage(),
+          SettingsPage.id: (context) => SettingsPage(),
+          SelectContactPage.id: (context) => SelectContactPage(),
+          AddDebtPage.id: (context) => AddDebtPage(
+                contact: Contact(avatar: '', fullname: '', id: ''),
+              ),
+
+          // Pages in drawer
+          ContactsPage.id: (context) => ContactsPage(),
+        },
       ),
-      home: RegistrationPage(),
-      routes: {
-        // Authentication
-        RegistrationPage.id: (context) => RegistrationPage(),
-        SignInPage.id: (context) => SignInPage(),
-        SignUpPage.id: (context) => SignUpPage(),
-
-        // Home
-        HomePage.id: (context) => HomePage(),
-        CreateContactPage.id: (context) => CreateContactPage(),
-        SettingsPage.id: (context) => SettingsPage(),
-        SelectContactPage.id: (context) => SelectContactPage(),
-        AddDebtPage.id: (context) => AddDebtPage(
-              contact: Contact(avatar: '', fullname: '', id: ''),
-            ),
-
-        // Pages in drawer
-        ContactsPage.id: (context) => ContactsPage(),
-      },
+      designSize: const Size(390, 844),
     );
   }
 }
--- lib/pages/authentication/registration_page.dart
+++ lib/pages/authentication/registration_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:qarz_app/pages/Authentication/signin_page.dart';
 import 'package:qarz_app/pages/Authentication/signup_page.dart';
+import 'package:qarz_app/services/utils_service.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RegistrationPage extends StatelessWidget {
   static final String id = 'registration_page';
@@ -30,7 +32,7 @@ class RegistrationPage extends StatelessWidget {
                 child: Container(
                   width: double.infinity,
                   height: fullHeight,
-                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                  padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -41,16 +43,16 @@ class RegistrationPage extends StatelessWidget {
                           Text(
                             'Welcome',
                             style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 30),
+                                fontWeight: FontWeight.bold, fontSize: 30.sp),
                           ),
                           SizedBox(
-                            height: 20,
+                            height: 20.h,
                           ),
                           Text(
                             'Automatic identity verification which enables you to verify your identity',
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                              fontSize: 15,
+                              fontSize: 15.sp,
                               color: Colors.grey[700],
                             ),
                           ),
@@ -74,12 +76,12 @@ class RegistrationPage extends StatelessWidget {
                         children: [
                           // Button : Login
                           MaterialButton(
-                            height: 70,
+                            height: 70.h,
                             minWidth: double.infinity,
                             child: Text(
                               'Login',
                               style: TextStyle(
-                                  fontWeight: FontWeight.w600, fontSize: 18),
+                                  fontWeight: FontWeight.w600, fontSize: 18.sp),
                             ),
                             onPressed: () {
                               Navigator.pushNamed(context, SignInPage.id);
@@ -88,18 +90,18 @@ class RegistrationPage extends StatelessWidget {
                                 side: BorderSide(
                                   color: Colors.black,
                                 ),
-                                borderRadius: BorderRadius.circular(50)),
+                                borderRadius: BorderRadius.circular(50.r)),
                           ),
 
                           SizedBox(
-                            height: 20,
+                            height: 20.h,
                           ),
 
                           // Button : Sign up
                           Container(
-                            padding: EdgeInsets.only(top: 3, left: 3),
+                            padding: EdgeInsets.only(top: 3.h, left: 3.w),
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
+                                borderRadius: BorderRadius.circular(50.r),
                                 border: Border(
                                   bottom: BorderSide(color: Colors.black),
                                   top: BorderSide(color: Colors.black),
@@ -107,14 +109,14 @@ class RegistrationPage extends StatelessWidget {
                                   right: BorderSide(color: Colors.black),
                                 )),
                             child: MaterialButton(
-                              height: 70,
+                              height: 70.h,
                               minWidth: double.infinity,
                               child: Text(
                                 'Sign up',
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontWeight: FontWeight.w600,
-                                    fontSize: 18),
+                                    fontSize: 18.sp),
                               ),
                               onPressed: () {
                                 Navigator.pushNamed(context, SignUpPage.id);
@@ -122,11 +124,68 @@ class RegistrationPage extends StatelessWidget {
                               color: Colors.grey[700],
                               elevation: 0,
                               shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50)),
+                                  borderRadius: BorderRadius.circular(50.r)),
                             ),
                           ),
                         ],
                       ),
+
+                      SizedBox(height: 25.h,),
+
+                      // Text : or
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Divider(height: 5.h, color: Colors.grey[700],),
+                          ),
+
+                          Text('  or  ', style: TextStyle(fontFamily: 'Consolas', color: Colors.grey[700])),
+
+                          Expanded(
+                            child: Divider(height: 5.h, color: Colors.grey[700],),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 20.h,),
+
+                      // IconButtons : Google || Apple
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          // Button : Google
+                          GestureDetector(
+                            onTap: () {
+                              Utils.fireToast('Not available yet!');
+                            },
+                            child: Image(
+                              height: 65.h,
+                              width: 65.w,
+                              color: Colors.grey[700],
+                              image: Svg(
+                                'assets/icons/social_icons/google-logo.svg',
+                              ),
+                            ),
+                          ),
+
+                          // Button : Apple
+                          GestureDetector(
+                            onTap: () {
+                              Utils.fireToast('Not available yet!');
+                            },
+                            child: Image(
+                              height: 65.h,
+                              width: 65.w,
+                              color: Colors.grey[700],
+                              image: Svg(
+                                'assets/icons/social_icons/apple-logo.svg',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                     // SizedBox(height: 60.h,),
                     ],
                   ),
                 ),
